f2bc85b5a5f2b367541c68cc766f133dacb339d7
Li Wang <li.wang@square-sense.com>
2020-05-16 23:41:13 +0200

    bugfix: circleci config

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
 
-  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.1.0"
+  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.1.1"
   lazy val slf4j = "com.typesafe.scala-logging" %% "scala-logging" % "3.9.2"
   lazy val logback = "ch.qos.logback" % "logback-classic" % "1.2.3"
 }
